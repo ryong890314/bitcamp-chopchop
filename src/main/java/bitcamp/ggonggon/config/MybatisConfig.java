73c5d5c1309a3d2be543fdcd4d8bcad77942ba39
sohansaem <iuehakdrk@gmail.com>
2019-10-16 18:05:11 +0900

    config 파일 공유

---
+++ src/main/java/bitcamp/ggonggon/config/MybatisConfig.java
@@ -0,0 +1,30 @@
+package bitcamp.ggonggon.config;
+
+import javax.sql.DataSource;
+import org.apache.ibatis.logging.LogFactory;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+//@Configuration
+@MapperScan("bitcamp.ggonggon.dao")
+public class MybatisConfig {
+  
+  @Bean
+  public SqlSessionFactory sqlSessionFactory(
+      DataSource dataSource, ApplicationContext appCtx) throws Exception {
+
+    // Mybatis의 Log4j2를 활성화시키기 
+    LogFactory.useLog4J2Logging();
+    
+    SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
+    sqlSessionFactoryBean.setDataSource(dataSource);
+    sqlSessionFactoryBean.setTypeAliasesPackage("bitcamp.ggonggon.domain");
+    sqlSessionFactoryBean.setMapperLocations(
+        appCtx.getResources("classpath:bitcamp/ggonggon/mapper/*Mapper.xml"));
+    
+    return sqlSessionFactoryBean.getObject();
+  }
+}
