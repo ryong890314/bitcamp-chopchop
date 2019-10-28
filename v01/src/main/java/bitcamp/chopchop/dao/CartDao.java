f1b1760aab506236e208f64f0f28118b63bf2e21
ginny <kdjj214@nate.com>
2019-10-28 20:57:48 +0900

    handlebars 적용

---
+++ v01/src/main/java/bitcamp/chopchop/dao/CartDao.java
@@ -0,0 +1,14 @@
+package bitcamp.chopchop.dao;
+
+import java.util.List;
+
+import bitcamp.chopchop.domain.Cart;
+
+public interface CartDao {
+  int insert(Cart cart) throws Exception;
+  List<Cart> findAll() throws Exception;
+  List<Cart> findByKeyword(String species) throws Exception;
+  Cart findBy(int no) throws Exception;
+  int update(Cart cart) throws Exception;
+  int delete(int no) throws Exception;
+}
